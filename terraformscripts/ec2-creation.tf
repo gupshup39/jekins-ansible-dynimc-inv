fc42e66bc46553fe752e1fa227847cc27e7f527a
archithag <99272526+archithag@users.noreply.github.com>
2023-12-21 13:36:17 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -5,7 +5,7 @@ region = "us-east-1"
 resource "aws_instance"  "AWSInstance"{
  ami = "ami-048d7f5ca8e2edd06"
  instance_type = "t4g.nano"
- key_name = "mtpdevops"
+ key_name = "archkey"
  security_groups = ["launch-wizard-2"]
  tags = {
  Name = "tomcatservers"
