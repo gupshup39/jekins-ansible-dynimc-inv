af8317d0dfe18c2962e5d44662356422588ec613
archithag <99272526+archithag@users.noreply.github.com>
2023-12-21 13:20:54 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-region = "ap-south-1"
+region = "us-east-1"
 }
 
 resource "aws_instance"  "AWSInstance"{
- ami = "ami-02a2af70a66af6dfb"
+ ami = "ami-02cd6549baea35b55"
  instance_type = "t2.micro"
  key_name = "mtpdevops"
  security_groups = ["launch-wizard-2"]
